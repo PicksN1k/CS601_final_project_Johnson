1f6232f9818c2a2a70dcf8fa0a63b6794b8bed27
PicksN1k <csjohnson2838@gmail.com>
2025-12-07 16:08:27 -0500

    modified:   projects/dist/projects.js

---
+++ projects/dist/projects.js
@@ -0,0 +1,16 @@
+import { projectData } from "./projectData";
+
+const container = document.getElementById("projectsContainer");
+
+projectData.forEach((p) => {
+  const card = document.createElement("div");
+  card.className = "project-card";
+
+  card.innerHTML = `
+    <h3>${p.title}</h3>
+    <p class="tech">${p.tech}</p>
+    <p>${p.desc}</p>
+  `;
+
+  container.appendChild(card);
+});
